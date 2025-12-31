dfb823a975d27bdd34f754772bc25352fb6d758d
Yasmin Farisah Salma <yasminfsalma@gmail.com>
2026-01-01 02:06:54 +0700

    Initial Commit

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Yasmin Farisah Salma on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
